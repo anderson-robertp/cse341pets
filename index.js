9e6c10ec639a52308929cccf2f2f6784499d86dc
anderson-robertp <anderon-rp@hotmail.com>
2025-02-05 19:02:30 -0600

    intital

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongodb = require('./db/connect');
+const petRoute = require('./routes/petRoute');
+const userRoute = require('./routes/userRoute');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const app = express();
+const port = process.env.port || 8080;
+
+app.use(cors()); // Enable CORS
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Swagger UI
+
+app // Main App
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/pets', profRoute)
+  .use('/users', contactRoute);
+
+// MongoDB Connection
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
---
+++ swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'Project for CSE341',
+  },
+  host: 'cse341-1ptd.onrender.com/contacts'
+};
+
+const outputFile = './swagger.json';
+const routes = ['./routes/contacts.js'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
